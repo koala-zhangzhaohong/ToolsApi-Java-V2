f07926aff97fdf05e6e300c9c9bedd5a40a5e86e
koala <owen000814@outlook.com>
2026-06-17 02:59:59 +0800

    feat: update

--- tools-web/src/main/java/com/koala/web/config/FilterConfig.java
+++ tools-web/src/main/java/com/koala/web/config/FilterConfig.java
@@ -1,6 +1,5 @@
 package com.koala.web.config;
 
-import com.koala.service.custom.filter.CustomCharacterEncodingFilter;
 import com.koala.web.filter.RequestLoggingFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -18,16 +17,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @DependsOn({"beanContext"})
 public class FilterConfig {
-
-    @Bean
-    public FilterRegistrationBean<CustomCharacterEncodingFilter> registrationCharsetEncodingFilter() {
-        FilterRegistrationBean<CustomCharacterEncodingFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CustomCharacterEncodingFilter());
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setName("customCharacterEncodingFilter");
-        registrationBean.setOrder(0);
-        return registrationBean;
-    }
 
     @Bean
     public FilterRegistrationBean<RequestLoggingFilter> registrationRequestLoggingFilter() {
