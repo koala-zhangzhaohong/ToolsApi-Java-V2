9dd328096c1791199664a33176ec6875b3859e47
koala <owen000814@outlook.com>
2026-06-17 04:30:32 +0800

    feat: update

--- tools-service/src/main/java/com/koala/service/utils/GsonUtil.java
+++ tools-service/src/main/java/com/koala/service/utils/GsonUtil.java
@@ -3,15 +3,7 @@ package com.koala.service.utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.ToNumberPolicy;
-import com.google.gson.TypeAdapter;
-import com.google.gson.internal.LinkedTreeMap;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -64,83 +56,24 @@ public class GsonUtil {
     /**
      * json字符串转成list
      */
+    @SuppressWarnings("unchecked")
     public static <T> List<T> toList(String gsonString) {
-        Gson gson = new GsonBuilder().registerTypeAdapter(new TypeToken<List<T>>() {
-        }.getType(), new MapTypeAdapter()).create();
-        return gson.fromJson(gsonString, new TypeToken<List<T>>() {
-        }.getType());
+        return GSON.fromJson(gsonString, List.class);
     }
 
     /**
      * json字符串转成list中有map的
      */
+    @SuppressWarnings("unchecked")
     public static <T> List<Map<String, T>> toListMaps(String gsonString) {
-        Gson gson = new GsonBuilder().registerTypeAdapter(new TypeToken<List<Map<String, T>>>() {
-        }.getType(), new MapTypeAdapter()).create();
-        return gson.fromJson(gsonString, new TypeToken<List<Map<String, T>>>() {
-        }.getType());
+        return GSON.fromJson(gsonString, List.class);
     }
 
     /**
      * json字符串转成map的
      */
+    @SuppressWarnings("unchecked")
     public static <T> Map<String, T> toMaps(String gsonString) {
-        Gson gson = new GsonBuilder().registerTypeAdapter(new TypeToken<Map<String, T>>() {
-        }.getType(), new MapTypeAdapter()).create();
-        return gson.fromJson(gsonString, new TypeToken<Map<String, T>>() {
-        }.getType());
-    }
-
-    public static class MapTypeAdapter extends TypeAdapter<Object> {
-
-        @Override
-        public Object read(JsonReader in) throws IOException {
-            JsonToken token = in.peek();
-            switch (token) {
-                case BEGIN_ARRAY:
-                    List<Object> list = new ArrayList<>();
-                    in.beginArray();
-                    while (in.hasNext()) {
-                        list.add(read(in));
-                    }
-                    in.endArray();
-                    return list;
-                case BEGIN_OBJECT:
-                    Map<String, Object> map = new LinkedTreeMap<>();
-                    in.beginObject();
-                    while (in.hasNext()) {
-                        map.put(in.nextName(), read(in));
-                    }
-                    in.endObject();
-                    return map;
-                case STRING:
-                    return in.nextString();
-                case NUMBER:
-                    /*
-                      改写数字的处理逻辑，将数字值分为整型与浮点型。
-                     */
-                    double dbNum = in.nextDouble();
-                    // 数字超过long的最大值，返回浮点类型
-                    if (dbNum > Long.MAX_VALUE) {
-                        return dbNum;
-                    }
-                    // 判断数字是否为整数值
-                    long lngNum = (long) dbNum;
-                    return dbNum;
-                case BOOLEAN:
-                    return in.nextBoolean();
-                case NULL:
-                    in.nextNull();
-                    return null;
-                default:
-                    throw new IllegalStateException();
-            }
-        }
-
-        @Override
-        public void write(JsonWriter out, Object value) throws IOException {
-            // 序列化无需实现
-        }
-
+        return GSON.fromJson(gsonString, Map.class);
     }
 }
