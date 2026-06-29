6debe09a551b4b99ead38458a3725b03c279d1fe
koala <owen000814@outlook.com>
2026-06-30 00:41:58 +0800

    feat: update

--- tools-factory/src/main/java/com/koala/factory/secretKey/KugouSecretKeyCollector.java
+++ tools-factory/src/main/java/com/koala/factory/secretKey/KugouSecretKeyCollector.java
@@ -4,5 +4,4 @@ public class KugouSecretKeyCollector {
     public static final String KUGOU_ITEM_SECRET_KEY = "OIlwieks28dk2k092lksi2UIkp";
     public static final String KUGOU_SEARCH_SECRET_KEY = "NVPh5oo715z5DIWAeQlhMDsWXXQV4hwt";
     public static final String KUGOU_PID_VERSION_SECRET_KEY = "57ae12eb6890223e355ccfcb74edf70d";
-    // not work
 }
